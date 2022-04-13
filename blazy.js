c1f8e405cfdb2a63fe9ec76f825eb165fd6f7c5e
Jeffrey Kondas <jeff.kondas@gmail.com>
2022-04-13 02:30:24 -0400

    Update blazy.js

--- blazy.js
+++ blazy.js
@@ -1,6 +1,7 @@
 /*!
-  hey, [be]Lazy.js - v1.8.2 - 2016.10.25
-  A fast, small and dependency free lazy load script (https://github.com/dinbror/blazy)
+  hey, [be]Lazier.js - v1.1.1 - 2022.04.13
+  Is Blazy the best way still??????????? (https://github.com/jkondas/)
+  From: A fast, small and dependency free lazy load script (https://github.com/dinbror/blazy)
   (c) Bjoern Klinggaard - @bklinggaard - http://dinbror.dk/blazy
 */
 ;
